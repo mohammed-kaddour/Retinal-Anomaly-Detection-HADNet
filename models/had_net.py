bcece5e84da01e667446c235c04ab324a78e6167
mohammed-kaddour <mohammed.kaddour@univ-temouchent.edu.dz>
2026-01-16 10:25:17 +0100

    Update had_net.py

--- models/had_net.py
+++ models/had_net.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 import torchvision.models as models
 
-# --- PROPOSED HYBRID MODEL ---
 class HybridAnomalyNet(nn.Module):
     def __init__(self, handcrafted_input_size=16, projection_dim=128):
         super(HybridAnomalyNet, self).__init__()
@@ -28,20 +27,3 @@ class HybridAnomalyNet(nn.Module):
         text_feat = self.texture_net(handcrafted)
         combined = torch.cat((deep_feat, text_feat), dim=1)
         return self.projection_head(self.fusion_layer(combined))
-
-# --- BASELINE MODEL ---
-class DeepOnlyAnomalyNet(nn.Module):
-    def __init__(self, projection_dim=128):
-        super(DeepOnlyAnomalyNet, self).__init__()
-        resnet = models.resnet18(pretrained=True)
-        self.backbone = nn.Sequential(*list(resnet.children())[:-1]) 
-        self.projection_head = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(512, 256),
-            nn.ReLU(),
-            nn.Linear(256, projection_dim)
-        )
-        
-    def forward(self, images):
-        deep_feat = self.backbone(images)
-        return self.projection_head(deep_feat)
