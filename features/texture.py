cf69b6a249c8ed62effe98c1c22fc2d522d77f91
mohammed-kaddour <mohammed.kaddour@univ-temouchent.edu.dz>
2026-01-16 09:56:06 +0100

    Update texture.py

--- features/texture.py
+++ features/texture.py
@@ -1,1 +1,20 @@
+import numpy as np
+from skimage.feature import graycomatrix, graycoprops, local_binary_pattern
 
+def extract_haralick(gray_img):
+    glcm = graycomatrix(gray_img, distances=[1], angles=[0, np.pi/4, np.pi/2, 3*np.pi/4], 
+                        levels=256, symmetric=True, normed=True)
+    return [
+        graycoprops(glcm, 'contrast').mean(),
+        graycoprops(glcm, 'dissimilarity').mean(),
+        graycoprops(glcm, 'homogeneity').mean(),
+        graycoprops(glcm, 'energy').mean(),
+        graycoprops(glcm, 'correlation').mean(),
+        graycoprops(glcm, 'ASM').mean()
+    ]
+
+def extract_lbp(gray_img, P=8, R=1):
+    lbp = local_binary_pattern(gray_img, P, R, method="uniform")
+    n_bins = int(lbp.max() + 1)
+    hist, _ = np.histogram(lbp.ravel(), bins=n_bins, range=(0, n_bins), density=True)
+    return hist.tolist()
