9928ecfd87996df9cfbb01c83533373b41ae6bf5
mohammed-kaddour <mohammed.kaddour@univ-temouchent.edu.dz>
2026-01-16 09:57:13 +0100

    Update dataset.py

--- data/dataset.py
+++ data/dataset.py
@@ -1,1 +1,25 @@
+import torch
+import cv2
+from torch.utils.data import Dataset
 
+class HybridAnomalyDataset(Dataset):
+    def __init__(self, df, features_array, transform=None):
+        self.df = df
+        self.features = features_array
+        self.transform = transform
+        
+    def __len__(self):
+        return len(self.df)
+    
+    def __getitem__(self, idx):
+        img_path = self.df.iloc[idx]['processed_path']
+        image = cv2.imread(img_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        
+        if self.transform:
+            image = self.transform(image)
+            
+        handcrafted = torch.tensor(self.features[idx], dtype=torch.float32)
+        label = torch.tensor(self.df.iloc[idx]['anomaly_label'], dtype=torch.float32)
+        
+        return image, handcrafted, label
